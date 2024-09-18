9bdbd53be98894bf17606945786c430d1cdf039b
vangoghpell <142349531+vangoghpell@users.noreply.github.com>
2024-09-18 11:21:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fantasia',
+    'Quem é a autora da saga Harry potter?',
+    'J.K. Rowling '
+)
+
+criaCartao(
+    'Suspense',
+    'Em a paciente silenciosa, quem matou o marido de Alicia?',
+    'Theo, o psiquiatra'
+)
+
+criaCartao(
+    'Romance',
+    'Qual o nome da filha de Lily em É assim que acaba?',
+    'Emerson'
+)
+
+criaCartao(
+    'Ficção',
+    'Qual o nome do acampamebnto de Percy Jackson?',
+    'Acampamento meio sangue '
+)
